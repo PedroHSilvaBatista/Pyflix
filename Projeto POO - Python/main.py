3fa5802e8fab4aad94c514a967f73f57f1ed41e9
Pedro <pedrohenriquesb02@gmail.com>
2024-12-10 18:48:57 -0300

    Documentando o código que contém a API

--- Projeto POO - Python/main.py
+++ Projeto POO - Python/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import os, json
 
+# Caminhos que levam até seus respectivos arquivos
 diretorio_atual = os.path.dirname(__file__)
 caminho_usuarios = os.path.join(diretorio_atual, 'data', 'usuarios.json')
 caminho_filmes = os.path.join(diretorio_atual, 'data', 'filmes.json')
@@ -9,18 +10,26 @@ caminho_documentarios = os.path.join(diretorio_atual, 'data', 'documentarios.jso
 
 app = FastAPI()
 
+# Estrutura que armazena os dados dos usuários em uma váriavel
 with open(caminho_usuarios, 'r', encoding='utf-8') as arquivo_usuarios:
     dados_usuarios_api = json.load(arquivo_usuarios)
 
+# Estrutura que armazena os dados dos filmes em uma váriavel
 with open(caminho_filmes, 'r', encoding='utf-8') as arquivo_filmes:
     dados_filmes_api = json.load(arquivo_filmes)
 
+# Estrutura que armazena os dados das séries em uma váriavel
 with open(caminho_series, 'r', encoding='utf-8') as arquivo_series:
     dados_series_api = json.load(arquivo_series)
 
+# Estrutura que armazena os dados dos documentários em uma váriavel
 with open(caminho_documentarios, 'r', encoding='utf-8') as arquivo_documentarios:
     dados_documentarios_api = json.load(arquivo_documentarios)
 
+
+@app.get('/')
+def status_api():
+    return {"hello": "world"}
 
 @app.get('/api/usuarios/')
 def usuarios():
